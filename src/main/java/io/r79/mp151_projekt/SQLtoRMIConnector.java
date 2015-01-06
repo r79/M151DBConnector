d68a764d53aefd3aea5feb4454b5895d107cdee7
R79 <r@r79.io>
2015-01-06 22:43:12 +0100

    !PROBABLY NOT WORKING QUICKSAVE VERSION

---
+++ src/main/java/io/r79/mp151_projekt/SQLtoRMIConnector.java
@@ -0,0 +1,70 @@
+package io.r79.mp151_projekt;
+
+import io.r79.mp151_projekt.dao.MySQLDAO;
+import io.r79.mp151_projekt.dto.PerformanceDTO;
+import io.r79.mp151_projekt.dto.VisitorDTO;
+import io.r79.mp151_projekt.rmiInterface.FilmClubInterface;
+
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by rbuechle on 03.12.2014.
+ */
+public class SQLtoRMIConnector extends UnicastRemoteObject implements FilmClubInterface {
+    private MySQLDAO dao = new MySQLDAO("url");
+
+    public SQLtoRMIConnector() throws RemoteException {
+    }
+
+    @Override
+    public ArrayList<PerformanceDTO> getPerformances(Date start, Date end) throws RemoteException {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+
+		System.out.println("incoming Request | Performances | start= " + sdf.format(start) + " | end= " + sdf.format(end) + " |");
+
+		ArrayList<PerformanceDTO> performances = dao.getPerformancesInRange(start, end);
+
+		System.out.println("responded " + performances.size() + " Performances");
+
+		return performances;
+    }
+
+    @Override
+    public ArrayList<VisitorDTO> getVisitors(int id) throws RemoteException {
+
+		System.out.println("incoming Request | Visitors | id= " + id + " |");
+
+		ArrayList<VisitorDTO> visitors = dao.getVisitors(id);
+
+		System.out.println("responded " + visitors.size() + " Visitors");
+
+        return visitors;
+
+    }
+    
+    public static void main(String[] args) {
+		try
+		{
+			try {
+				LocateRegistry.createRegistry(1099);
+			} catch (RemoteException e) {
+				System.out.println("Could not start Registry");
+			}
+			
+			SQLtoRMIConnector impl = new SQLtoRMIConnector();
+			
+			String SQLService = "SQLService";
+			Naming.rebind("//localhost/" + SQLService, impl);
+		}
+		catch(Exception exc) {
+			exc.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/io/r79/mp151_projekt/dao/MySQLDAO.java
@@ -0,0 +1,84 @@
+package io.r79.mp151_projekt.dao;
+
+import io.r79.mp151_projekt.dto.PerformanceDTO;
+import io.r79.mp151_projekt.dto.VisitorDTO;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * Created by rbuechle on 03.12.2014.
+ */
+public class MySQLDAO {
+
+    private Connection connection = null;
+
+    public MySQLDAO(String url) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/filmclub", "root", "");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<PerformanceDTO> getPerformancesInRange(Date start, Date end) {
+        ArrayList<PerformanceDTO> performances = new ArrayList<PerformanceDTO>();
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    String.format("SELECT veranstaltung.VeranstaltungID, UNIX_TIMESTAMP(veranstaltung.Datum), raum.Bezeichnung, film.Titel " +
+                            "FROM veranstaltung, raum, film " +
+                            "WHERE veranstaltung.FilmIDFS = film.FilmID " +
+                            "AND %s < UNIX_TIMESTAMP(veranstaltung.datum) " +
+                            "AND UNIX_TIMESTAMP(veranstaltung.datum) < %s " +
+                            "AND veranstaltung.raumIDFS = raum.RaumID " +
+                            "ORDER BY veranstaltung.Datum;", start.getTime()/1000, end.getTime()/1000));
+            ResultSet rs = statement.executeQuery();
+
+            while(rs.next()) {
+                performances.add(new PerformanceDTO(rs.getInt(1), new Date(rs.getInt(2)), rs.getString(3), rs.getString(4), ""));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return performances;
+    }
+
+    public ArrayList<VisitorDTO> getVisitors(int performanceId) {
+        ArrayList<VisitorDTO> visitors = new ArrayList<VisitorDTO>();
+        try {
+            PreparedStatement statement = connection.prepareStatement(
+                    String.format("SELECT mitglied.PersID, mitglied.Vorname, mitglied.Name, mitglied.Privattelefon " +
+                            "FROM mitglied, besuch " +
+                            "WHERE besuch.VeranstaltungID =%d " +
+                            "AND besuch.mitgliedID = mitglied.PersID " +
+                            "ORDER BY mitglied.Vorname;", performanceId)
+            );
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                visitors.add(new VisitorDTO(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4)));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return visitors;
+    }
+
+
+    public static void main(char[] args) {
+
+    }
+}
